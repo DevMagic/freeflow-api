6530c1c34c92094590bed8d97f4ebcaa7f7db0ec
Claudio Oliveira <claudio_pg-sp@hotmail.com>
2022-02-14 10:38:42 -0300

    initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-
+import { configService } from './config/config';
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
