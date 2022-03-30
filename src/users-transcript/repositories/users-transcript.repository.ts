488cca3b36adf109c76b22dbcb3ac5e8234bdd71
Lucas Leonardi <leonardi.lucas@hotmail.com>
2022-03-30 15:40:04 -0300

    fix(*): adding fields to get user transcript return and camalcase

--- src/users-transcript/repositories/users-transcript.repository.ts
+++ src/users-transcript/repositories/users-transcript.repository.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { UsersTranscript } from '../entities/users-transcript.entity';
 import { CreateUsersTranscriptDto } from '../dto/create-users-transcript.dto';
-
+import * as camelcaseKeys from 'camelcase-keys';
 @Injectable()
 @EntityRepository(UsersTranscript)
 export class UsersTranscriptRepository extends Repository<UsersTranscript>{
@@ -31,14 +31,15 @@ export class UsersTranscriptRepository extends Repository<UsersTranscript>{
       filterQuery += `AND category = $2`
     }
 
-    let userTranscript = this.query(`SELECT category, amount, gratitude_type, created_at, 
+    let userTranscript = this.query(`SELECT category, amount, gratitude_type, created_at, transfer_action,
                                     (SELECT username from users u where u.id = ut.user_id) as username, 
-                                    (SELECT username from users u where u.id = ut.exchange_user_id) as exchangeUsername 
+                                    (SELECT username from users u where u.id = ut.exchange_user_id) as exchange_username,
+                                    (SELECT photo_url from users u where u.id = ut.exchange_user_id) as photo_url
                                     FROM users_transcript ut
                                     WHERE ${filterQuery}
                                     ORDER by ut.created_at DESC
                                     LIMIT 20  
                                     OFFSET ${filterOffset}`, params);
-    return await userTranscript;                        
+    return camelcaseKeys(await userTranscript);                        
   }
 }
